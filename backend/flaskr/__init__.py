ac23c6660f01b7a25413433dc016d88bd2a94f05
girgisdev <girgis.developer@yahoo.com>
2020-08-26 19:29:12 +0200

    feat: updated Question section to work properly
    - updated questions list
    - updated categories list
    - updated add question to work properly

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,24 +8,47 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start =  (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  cors = CORS(app, resources={r"/questions/*": {"origins": "*"}})
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization,true")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS")
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def retrieve_categories():
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = [category.format() for category in categories]
+
+    if len(categories) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'categories': formatted_categories
+    })
 
 
   '''
@@ -41,6 +64,24 @@ def create_app(test_config=None):
   Clicking on the page numbers should update the questions. 
   '''
 
+  @app.route('/questions')
+  def retrieve_questions():
+    questions = Question.query.order_by(Question.id).all()
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = [category.format() for category in categories]
+    current_questions = paginate_questions(request, questions)
+
+    if len(current_questions) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'categories': formatted_categories,
+      'current_category': 'a',
+      'total_questions': len(Question.query.all())
+    })
+
   '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
@@ -48,6 +89,22 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route("/questions/<int:question_id>", methods=["DELETE"])
+  def delete_question(question_id):
+    try:
+      question = Question.query.get(question_id)
+
+      if question is None:
+        abort(404)
+
+      question.delete()
+      return jsonify({
+        'success': True,
+        'deleted': question_id,
+      })
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -59,6 +116,25 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+  @app.route("/questions", methods=["POST"])
+  def add_question():
+    body = request.get_json()
+    question = body.get("question", "")
+    answer = body.get("answer", "")
+    category = body.get("category", "")
+    difficulty = body.get("difficulty", "")
+    
+    try:
+      question = Question(question = question, answer = answer, category = category, difficulty = difficulty)
+      question.insert()
+
+      return jsonify({
+        'success': True,
+        'created': question.id
+      })
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -79,6 +155,23 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that 
   category to be shown. 
   '''
+  @app.route("/categories/<int:category_id>/questions")
+  def get_questions_by_category(category_id):
+    questions = Question.query.filter(Question.category == category_id).all()
+    categories = Category.query.order_by(Category.id).all()
+    formatted_categories = [category.format() for category in categories]
+    current_questions = paginate_questions(request, questions)
+
+    if len(current_questions) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'categories': formatted_categories,
+      'current_category': 'a',
+      'total_questions': len(Question.query.all())
+    })
 
 
   '''
@@ -98,6 +191,29 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False, 
+      "error": 404,
+      "message": "resource not found"
+    }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False, 
+      "error": 422,
+      "message": "unprocessable"
+    }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      "success": False, 
+      "error": 400,
+      "message": "bad request"
+    }), 400
   
   return app
 
--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -11,7 +11,7 @@ class FormView extends Component {
       answer: "",
       difficulty: 1,
       category: 1,
-      categories: {}
+      categories: []
     }
   }
 
@@ -87,13 +87,12 @@ class FormView extends Component {
             </select>
           </label>
           <label>
-            Category
             <select name="category" onChange={this.handleChange}>
-              {Object.keys(this.state.categories).map(id => {
-                  return (
-                    <option key={id} value={id}>{this.state.categories[id]}</option>
-                  )
-                })}
+              {this.state.categories && this.state.categories.map(category => {
+                return (
+                  <option key={category.id} value={category.id}>{category.type}</option>
+                )
+              })}
             </select>
           </label>
           <input type="submit" className="button" value="Submit" />
